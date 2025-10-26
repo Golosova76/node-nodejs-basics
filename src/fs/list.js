c46b13ee0849e5d2e1b2b8bf107d803c94b8f849
Anna Golosova <golosova76@gmail.com>
2025-10-26 15:03:05 +0300

    feat: implements function list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import { readdir } from "fs/promises";
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+
+const dirName = "files";
+const errorMessage = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const dirPath = join(__dirname, dirName);
+
+    try {
+        const files = await readdir(dirPath);
+        console.log(files);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error(errorMessage);
+        }
+        throw error;
+    }
 };
 
 await list();
