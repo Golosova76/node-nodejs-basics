2f7a32cc40a6e4d0d5ef1e675cb51f0180118267
Anna Golosova <golosova76@gmail.com>
2025-10-26 18:45:45 +0300

    feat: implements function streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import {fileURLToPath} from "node:url";
+import {dirname, join} from "node:path";
+import { createWriteStream } from 'node:fs';
+import {pipeline} from "node:stream/promises";
+
+const dirName = "files";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const nameFile = "fileToWrite.txt";
+const filePath = join(__dirname, dirName, nameFile);
+
+
 const write = async () => {
-  // Write your code here
+    const writeStream = createWriteStream(filePath);
+    await pipeline(process.stdin, writeStream);
 };
 
 await write();
